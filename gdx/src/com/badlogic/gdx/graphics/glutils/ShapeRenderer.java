8a4ae879e11bcebe80f4ff2ed5908c04a254da5f
hacker1024 <joshleivenzon@outlook.com>
2017-06-15 09:46:33 +0300

    Create ShapeRenderer.java
    Allowed filled polygons

--- gdx/src/com/badlogic/gdx/graphics/glutils/ShapeRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ShapeRenderer.java
@@ -1100,35 +1100,65 @@ public class ShapeRenderer implements Disposable {
 	}
 
 	/** Draws a polygon in the x/y plane using {@link ShapeType#Line}. The vertices must contain at least 3 points (6 floats x,y). */
-	public void polygon (float[] vertices, int offset, int count) {
-		if (count < 6) throw new IllegalArgumentException("Polygons must contain at least 3 points.");
-		if (count % 2 != 0) throw new IllegalArgumentException("Polygons must have an even number of vertices.");
+	EarClippingTriangulator ear = new EarClippingTriangulator();
 
-		check(ShapeType.Line, null, count);
-		float colorBits = color.toFloatBits();
-		float firstX = vertices[0];
-		float firstY = vertices[1];
+	public void polygon(float[] vertices, int offset, int count)
+	{
+    if (shapeType != ShapeType.Filled && shapeType != ShapeType.Line)
+        throw new GdxRuntimeException("Must call begin(ShapeType.Filled) or begin(ShapeType.Line)");
+    if (count < 6)
+        throw new IllegalArgumentException("Polygons must contain at least 3 points.");
+    if (count % 2 != 0)
+        throw new IllegalArgumentException("Polygons must have an even number of vertices.");
 
-		for (int i = offset, n = offset + count; i < n; i += 2) {
-			float x1 = vertices[i];
-			float y1 = vertices[i + 1];
+    check(shapeType, null, count);
 
-			float x2;
-			float y2;
+    final float firstX = vertices[0];
+    final float firstY = vertices[1];
+    if (shapeType == ShapeType.Line)
+    {
+        for (int i = offset, n = offset + count; i < n; i += 2)
+        {
+            final float x1 = vertices[i];
+            final float y1 = vertices[i + 1];
 
-			if (i + 2 >= count) {
-				x2 = firstX;
-				y2 = firstY;
-			} else {
-				x2 = vertices[i + 2];
-				y2 = vertices[i + 3];
-			}
+            final float x2;
+            final float y2;
 
-			renderer.color(colorBits);
-			renderer.vertex(x1, y1, 0);
-			renderer.color(colorBits);
-			renderer.vertex(x2, y2, 0);
+            if (i + 2 >= count)
+		    {
+			x2 = firstX;
+			y2 = firstY;
+		    } else
+		    {
+			x2 = vertices[i + 2];
+			y2 = vertices[i + 3];
+		    }
+
+		    renderer.color(color);
+		    renderer.vertex(x1, y1, 0);
+		    renderer.color(color);
+		    renderer.vertex(x2, y2, 0);
+
 		}
+	    } else
+	    {
+		ShortArray arrRes = ear.computeTriangles(vertices);
+
+		for (int i = 0; i < arrRes.size - 2; i = i + 3)
+		{
+		    float x1 = vertices[arrRes.get(i) * 2];
+		    float y1 = vertices[(arrRes.get(i) * 2) + 1];
+
+		    float x2 = vertices[(arrRes.get(i + 1)) * 2];
+		    float y2 = vertices[(arrRes.get(i + 1) * 2) + 1];
+
+		    float x3 = vertices[arrRes.get(i + 2) * 2];
+		    float y3 = vertices[(arrRes.get(i + 2) * 2) + 1];
+
+		    this.triangle(x1, y1, x2, y2, x3, y3);
+		}
+	    }
 	}
 
 	/** @see #polygon(float[], int, int) */
